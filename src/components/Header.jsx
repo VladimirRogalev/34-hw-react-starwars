21113459101a5dbec1aac971598b15e161d0f6c9
Vladimir <vladimirrogalev1@gmail.com>
2024-05-18 13:51:32 +0300

    third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,16 @@
 import React, {Component} from 'react';
+import Navigation from "./Navigation.jsx";
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-        const { changePage } = this.props;
         return (
             <header>
-                <nav className="fixed-top mt-1 ms-5">
-                    <ul className="nav">
-                        <li className="nav-item btn btn-danger mx-1" onClick={() => changePage('Home')}>Home</li>
-                        <li className="nav-item btn btn-danger mx-1" onClick={() => changePage('AboutMe')}>About me</li>
-                        <li className="nav-item btn btn-danger mx-1" onClick={() => changePage('StarWars')}>Star Wars</li>
-                        <li className="nav-item btn btn-danger mx-1" onClick={() => changePage('Contacts')}>Contact</li>
-                    </ul>
-                </nav>
+                <Navigation changePage={this.props.changePage}/>
                 <h1 className="text-center py-4">Luke Skywalker</h1>
-
             </header>
 
         );
